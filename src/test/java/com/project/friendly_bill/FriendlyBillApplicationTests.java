49a9d249fe486c917a86686ee03629f76dda5c3e
abcdebug._ <quocnamnehehe@gmail.com>
2025-10-12 10:00:02 +0700

    init project

---
+++ src/test/java/com/project/friendly_bill/FriendlyBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.friendly_bill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
